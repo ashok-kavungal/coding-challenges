48f1c754f9cf54d4d00d0f062ceb48104f364007
ashokkavungal5@gmail.com <ashokkavungal5@gmail.com>
2020-08-12 13:49:23 +0200

    problem 7

---
+++ problems/07/solution_07.js
@@ -0,0 +1,17 @@
+  const maxSubarraySum = (arr, num) => {
+      let maxSum = 0;
+      let tempSum = 0;
+      if (arr.length < num) return null;
+
+      for (let i = 0; i < num; i++) {
+          maxSum += arr[i];
+      }
+      tempSum = maxSum;
+      for (let i = num; i < arr.length; i++) {
+          tempSum = tempSum - arr[i - num] + arr[i];
+          maxSum = Math.max(maxSum, tempSum);
+      }
+      return maxSum;
+  }
+
+  console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) )// 17
