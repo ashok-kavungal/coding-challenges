beca24d6bdf5f1d51f04b3fc52512e27af185915
ashokkavungal5@gmail.com <ashokkavungal5@gmail.com>
2020-08-26 15:41:52 +0200

    refactored fibnocci series

--- problems/08/solution_08.js
+++ problems/08/solution_08.js
@@ -1,6 +1,44 @@
-const fib = n =>{
+//old school approoch
+const fib_old= n =>{
     if (n <= 2) return 1;
-    return fib(n-1) + fib(n-2);
+    return fib_old(n-1) + fib_old(n-2);
 }
 
-console.log(fib(35)); // 9227465
+//using memoisation
+const fib_memo= n=>{
+    if(n <= 2) return 1;
+    var fibNums = [0,1,1];
+    for(var i = 3; i <= n; i++){
+        fibNums[i] = fibNums[i-1] + fibNums[i-2];
+    }
+    return fibNums[n];
+}
+
+//using tabulation
+const fib_tab = n =>{
+    if(n <= 2) return 1;
+    var fibNums = [0,1,1];
+    for(var i = 3; i <= n; i++){
+        fibNums[i] = fibNums[i-1] + fibNums[i-2];
+    }
+    return fibNums[n];
+}
+
+
+const fib_input = 35; //9227465
+
+let start = new Date();
+const fib_old_result = fib_old(fib_input);
+let exec_time = new Date() - start;
+console.info('Old school method: Result is: '+ fib_old_result +' ,Execution time: %dms',exec_time);
+
+let start1 = new Date();
+const fib_memo_result = fib_memo(fib_input);
+let exec_time1 = new Date() - start1;
+console.info('Memoization method: Result is: '+ fib_memo_result +' ,Execution time: %dms',exec_time1);
+
+let start2 = new Date();
+const fib_tab_result = fib_tab(fib_input);
+let exec_time2 = new Date() - start2;
+console.info('Tabulation method: Result is: '+ fib_tab_result +' ,Execution time: %dms',exec_time2);
+
